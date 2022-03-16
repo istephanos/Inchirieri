ef4c3dd92d22abcda9568a242c234a77bd6811cc
stefa <stefa@DESKTOP-GQEI76D>
2022-03-16 08:15:35 +0200

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inchirieri
+{
+    internal class Client
+    {
+        string nume,prenume;
+        string cnp, data_start, data_end;
+        
+        //	Constructor fara parametri
+        public Client()
+        {
+            Console.WriteLine("Constructor fara parametri");
+            Console.ReadKey();
+            nume = string.Empty;
+            prenume = string.Empty;
+            cnp = string.Empty;
+            data_start = string.Empty;
+            data_end = string.Empty;
+        }
+
+        //	Constructor fara parametri
+        public Client(string _nume,string _prenume,string _cnp, string _data_start,string _data_end)
+        {
+            nume = _nume;
+            prenume =_prenume;
+            cnp =_cnp;
+            data_start =_data_start;
+            data_end =_data_end;
+        }
+
+    }
+}
---
+++ Masini.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inchirieri
+{
+    internal class Masini
+    {
+        string firma;
+        string model;
+        string an_fabricatie;
+        string culoare;
+        //bool disponibilitate;
+
+        //constructor fara parametri
+        public Masini()
+        {
+            firma =string.Empty;
+            model = string.Empty;
+            an_fabricatie = string.Empty;
+            culoare = string.Empty;
+        }
+
+        //constructor cu parametri
+
+        public Masini(string _firma, string _model, string _an_fabricatie,string _culoare)
+        {
+            firma = _firma;
+            model = _model;
+            an_fabricatie = _an_fabricatie;
+            culoare = _culoare;
+        }
+    }
+}
